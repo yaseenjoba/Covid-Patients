c239a4675331d2442bf12f59a9e9681acbec8cfc
yaseenjoba <yaseenjoba0@gmail.com>
2023-06-30 20:24:30 +0300

    Initial Commit

---
+++ Covid Patients/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Covid Patients
+//
+//  Created by Yassen Joba on 30/06/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
